56443184382045653eb274259933d49f5da2cf73
ForexOL <zantamore@gmail.com>
2025-05-01 23:23:18 +0300

    up

--- flask_app.py
+++ flask_app.py
@@ -126,6 +126,7 @@ def get_download_link():
 
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.run(host="0.0.0.0", port=5000)
 
 
